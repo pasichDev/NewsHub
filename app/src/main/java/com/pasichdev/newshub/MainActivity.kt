515fa4f646b64e1ee61e916d64f2ae4065719cb8
PasichDev <67899666+pasichDev@users.noreply.github.com>
2023-04-19 22:26:00 +0300

    init

--- app/src/main/java/com/pasichdev/newshub/MainActivity.kt
+++ app/src/main/java/com/pasichdev/newshub/MainActivity.kt
@@ -3,13 +3,22 @@ package com.pasichdev.newshub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pasichdev.newshub.ui.theme.NewsHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +26,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NewsHubTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +39,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+    ) {
+        ToolbarMainScreen()
+        Text(
+            text = stringResource(id = R.string.chooseTag),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = modifier.padding(vertical = 30.dp)
+        )
+
+
+    }
 }
+
+
+@Composable
+fun ToolbarMainScreen(){
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.titleLarge
+        )
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(
+                text = stringResource(id = R.string.skip),
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NewsHubTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
--- app/src/main/java/com/pasichdev/newshub/ui/theme/Type.kt
+++ app/src/main/java/com/pasichdev/newshub/ui/theme/Type.kt
@@ -2,9 +2,20 @@ package com.pasichdev.newshub.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pasichdev.newshub.R
+val titleBoldFont = FontFamily(
+    Font(R.font.inter_bold, FontWeight.Bold)
+)
+val titleMediumFont = FontFamily(
+    Font(R.font.itim_regular, FontWeight.Bold)
+)
+val bodyFont = FontFamily(
+    Font(R.font.sansation_light, FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +25,21 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    titleLarge = TextStyle(
+        fontFamily = titleBoldFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = titleMediumFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
