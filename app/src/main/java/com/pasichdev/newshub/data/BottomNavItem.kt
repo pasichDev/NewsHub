f2b06e3d983cdffc121e617f08eb1563c26cb6bb
PasichDev <67899666+pasichDev@users.noreply.github.com>
2023-04-23 15:05:49 +0300

    #2
    Signed-off-by: PasichDev <67899666+pasichDev@users.noreply.github.com>

---
+++ app/src/main/java/com/pasichdev/newshub/data/BottomNavItem.kt
@@ -0,0 +1,11 @@
+package com.pasichdev.newshub.data
+
+import com.pasichdev.newshub.R
+
+sealed class BottomNavItem(var title: String, var icon: Int, var screen_route: String) {
+
+    object Home : BottomNavItem("Home", R.drawable.ic_home, "Home")
+    object Explore : BottomNavItem("Explore", R.drawable.ic_explore, "Explore")
+    object Saved : BottomNavItem("Saved", R.drawable.ic_saved, "Saved")
+    object Settings : BottomNavItem("Settings", R.drawable.ic_settings, "Settings")
+}
